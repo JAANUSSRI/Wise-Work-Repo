c42e5f31a634165c9bb3b4f6777d3c8461e18c34
JAANUSSRI <jaanussri2110611@ssn.edu.in>
2025-02-05 13:06:22 +0530

    2-Adding enum

--- Project1/SchoolHRAdministraton/Program.cs
+++ Project1/SchoolHRAdministraton/Program.cs
@@ -35,72 +35,80 @@ namespace SchoolHRadministraton
 
         public static void SeedData(List<IEmployee> employees)
         {
-            IEmployee teacher1 = new Teacher
-            {
-                Id = 1,
-                FName = "Jenny",
-                LName = "Thomas",
-                Salary = 50000
-            };
+            // IEmployee teacher1 = new Teacher
+            // {
+            //     Id = 1,
+            //     FName = "Jenny",
+            //     LName = "Thomas",
+            //     Salary = 50000
+            // };
+            IEmployee teacher1 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Jenny", "Thomas", 50000);
             employees.Add(teacher1);
-            IEmployee teacher2 = new Teacher
-            {
-                Id = 2,
-                FName = "Lucy",
-                LName = "Finsher",
-                Salary = 60000
-            };
+            // IEmployee teacher2 = new Teacher
+            // {
+            //     Id = 2,
+            //     FName = "Lucy",
+            //     LName = "Finsher",
+            //     Salary = 60000
+            // };
+            IEmployee teacher2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 2, "Lucy", "Finsher", 60000);
             employees.Add(teacher2);
-            IEmployee teacher3 = new Teacher
-            {
-                Id = 3,
-                FName = "Vilsa",
-                LName = "Ruther",
-                Salary = 40000
-            };
+            // IEmployee teacher3 = new Teacher
+            // {
+            //     Id = 3,
+            //     FName = "Vilsa",
+            //     LName = "Ruther",
+            //     Salary = 40000
+            // };
+            IEmployee teacher3 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 3, "Vilsa", "Ruther", 40000);
             employees.Add(teacher3);
-            IEmployee teacher4 = new Teacher
-            {
-                Id = 4,
-                FName = "Saira",
-                LName = "Begam",
-                Salary = 37000
-            };
+            // IEmployee teacher4 = new Teacher
+            // {
+            //     Id = 4,
+            //     FName = "Saira",
+            //     LName = "Begam",
+            //     Salary = 37000
+            // };
+            IEmployee teacher4 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 4, "Saira", "Begum", 37000);
             employees.Add(teacher4);
-            IEmployee teacher5 = new Teacher
-            {
-                Id = 5,
-                FName = "Vincy",
-                LName = "Robert",
-                Salary = 70000
-            };
+            // IEmployee teacher5 = new Teacher
+            // {
+            //     Id = 5,
+            //     FName = "Vincy",
+            //     LName = "Robert",
+            //     Salary = 70000
+            // };
+            IEmployee teacher5 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 5, "Vincy", "Robert", 70000); 
             employees.Add(teacher5);
 
-            IEmployee headofDept = new HeadOfDept
-            {
-                Id = 6,
-                FName = "Brenda",
-                LName = "Mullins",
-                Salary = 150000
-            };
+            // IEmployee headofDept = new HeadOfDept
+            // {
+            //     Id = 6,
+            //     FName = "Brenda",
+            //     LName = "Mullins",
+            //     Salary = 150000
+            // };
+            IEmployee headofDept = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadOfDept, 6, "Brenda", "Mullins", 150000); 
             employees.Add(headofDept);
 
-            IEmployee deputyHeadMaster = new DeputyHeadMaster
-            {
-                Id = 7,
-                FName = "Ferinda",
-                LName = "Roslin",
-                Salary = 70000
-            };
+            // IEmployee deputyHeadMaster = new DeputyHeadMaster
+            // {
+            //     Id = 7,
+            //     FName = "Ferinda",
+            //     LName = "Roslin",
+            //     Salary = 70000
+            // };
+            IEmployee deputyHeadMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.DeputyHeadMaster, 7, "Ferinda", "Roslin", 70000); 
             employees.Add(deputyHeadMaster);;
 
-            IEmployee headMaster = new HeadMaster
-            {
-                Id = 8,
-                FName = "Varsha",
-                LName = "Gnanam",
-                Salary = 170000
-            };
+            // IEmployee headMaster = new HeadMaster
+            // {
+            //     Id = 8,
+            //     FName = "Varsha",
+            //     LName = "Gnanam",
+            //     Salary = 170000
+            // };
+            IEmployee headMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadMaster, 8, "Varsha", "Gnanam", 170000); 
             employees.Add(headMaster);        
         }
     }
--- Project1/SchoolHRAdministraton/obj/Debug/net9.0/SchoolHRAdministraton.AssemblyInfo.cs
+++ Project1/SchoolHRAdministraton/obj/Debug/net9.0/SchoolHRAdministraton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolHRAdministraton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+202d70d0624556a6321b938967f19560d7194fc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolHRAdministraton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolHRAdministraton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
