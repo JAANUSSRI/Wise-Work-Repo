e354dda7089c6da7448e9df33a6230eb5a6313bd
JAANUSSRI <jaanussri2110611@ssn.edu.in>
2025-02-06 06:32:13 +0530

    6-Delegates-Reference-Instance-Methods

--- Project2/DelegatesBasic/Program.cs
+++ Project2/DelegatesBasic/Program.cs
@@ -7,24 +7,46 @@ namespace DelegatesBasic
         delegate void LogDel(string text);
         static void Main(string[] args)
         {
-            // LogDel logdel = new LogDel(LogTextToScreen); //Pass tne name of the method (like: LogTextToScreen) that we want the logdel variable to reference into the logdel types constructor
-            // logdel("Hi Jaanu!");   // or logdel.Invoke("Hi Jaanu!") - can also invoke delegates through the use of delegates types invoke method \
-            LogDel logdel = new LogDel(LogTextToFile); // now logdel variable referencing to file
-                                                       // creates log.txt file in Project2\DelegatesBasic\bin\Debug\net9.0\Log.txt
+        //     // LogDel logdel = new LogDel(LogTextToScreen); //Pass tne name of the method (like: LogTextToScreen) that we want the logdel variable to reference into the logdel types constructor
+        //     // logdel("Hi Jaanu!");   // or logdel.Invoke("Hi Jaanu!") - can also invoke delegates through the use of delegates types invoke method \
+        //     LogDel logdel = new LogDel(LogTextToFile); // now logdel variable referencing to file
+        //                                                // creates log.txt file in Project2\DelegatesBasic\bin\Debug\net9.0\Log.txt
+        //     Console.Write("Kindly Enter Your Name : ");
+        //     var name = Console.ReadLine();
+        //     logdel(name);
+        //     Console.ReadKey();
+        // }
+        // static void LogTextToScreen(string text){
+        //     Console.WriteLine($"{DateTime.Now} : Nice to see you {text}!");
+        // }
+        // static void LogTextToFile(string text)
+        // {
+        //     using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+        //     {
+        //         sw.WriteLine($"{DateTime.Now} : {text}");
+        //     }
+        // }
+            Log log = new Log();
+            // LogDel logdel = new LogDel(log.LogTextToScreen);
+            LogDel logdel = new LogDel(log.LogTextToFile);
             Console.Write("Kindly Enter Your Name : ");
             var name = Console.ReadLine();
             logdel(name);
-            Console.ReadKey();
-        }
-        static void LogTextToScreen(string text){
+            Console.ReadKey();     
+        }       
+    }
+    public class Log
+    {
+        public void LogTextToScreen(string text){
             Console.WriteLine($"{DateTime.Now} : Nice to see you {text}!");
         }
-        static void LogTextToFile(string text)
+        public void LogTextToFile(string text)
         {
             using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
             {
                 sw.WriteLine($"{DateTime.Now} : {text}");
             }
         }
+
     }
 }
